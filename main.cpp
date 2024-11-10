eb19349c8d030293998e9fa89967e31e2afd53a3
AlexPSvet <alejandroperezsvet@gmail.com>
2024-11-10 13:13:14 +0100

    Changed move fonctions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 vector<vector<int>> table;
 
+/* Fonction pour afficher un tableau d'entiers sur la console.
+ * @param un tableau d'entiers
+ */
 void afficherTableau(vector<int> tableau) {
     cout << "[";
     for (int i = 0; i < tableau.size(); i++) {
@@ -16,6 +19,9 @@ void afficherTableau(vector<int> tableau) {
     cout << "]";
 }
 
+/* Fonction pour afficher un tableau d'entiers à deux dimensions sur la console.
+ * @param un tableau d'entiers à deux dimensions
+ */
 void afficherTableau(vector<vector<int>> tableau) {
     cout << "[";
     for (int i = 0; i < tableau.size(); i++) {
@@ -50,8 +56,117 @@ void setRandomElement() {
         }
     }
     int k = (int) rand() % indexWithValues.size();
-    int j = emptySlots[k][(int) rand() % emptySlots[indexWithValues[k]].size()];
+    int i = indexWithValues[k];
+    int j = emptySlots[i][(int) rand() % emptySlots[i].size()];
     table[k][j] = 2;
+}
+
+void moveLeft() {
+    for (int i = 0; i < table.size(); i++) {
+        int first0Index = -1;
+        for (int j = 0; j < table[i].size(); j++) {
+            if (table[i][j] == 0) {
+                if (first0Index == -1) {
+                    first0Index = j;
+                }
+            } else if (first0Index != -1) {
+                int value = table[i][j];
+                table[i][j] = 0;
+                table[i][first0Index] = value;
+                first0Index += 1;
+            }
+        }
+    }
+    for (int i = 0; i < table.size(); i++) {
+        for (int j = table[i].size() - 1; j > 0; j--) {
+            if (table[i][j] != 0 && table[i][j-1] == table[i][j]) {
+                int value = table[i][j] * 2;
+                table[i][j] = 0;
+                table[i][j-1] = value;
+            }
+        }
+    }
+}
+
+void moveRight() {
+    for (int i = 0; i < table.size(); i++) {
+        int first0Index = -1;
+        for (int j = table[i].size() - 1; j >= 0; j--) {
+            if (table[i][j] == 0) {
+                if (first0Index == -1) {
+                    first0Index = j;
+                }
+            } else if (first0Index != -1) {
+                int value = table[i][j];
+                table[i][j] = 0;
+                table[i][first0Index] = value;
+                first0Index -= 1;
+            }
+        }
+    }
+    for (int i = 0; i < table.size(); i++) {
+        for (int j = 0; j < table[i].size() - 1; j++) {
+            if (table[i][j] != 0 && table[i][j+1] == table[i][j]) {
+                int value = table[i][j] * 2;
+                table[i][j] = 0;
+                table[i][j+1] = value;
+            }
+        }
+    }
+}
+
+void moveDown() {
+    for (int i = 0; i < 4; i++) {
+        int first0Index = -1;
+        for (int j = 3; j >= 0; j--) {
+            if (table[j][i] == 0) {
+                if (first0Index == -1) {
+                    first0Index = j;
+                }
+            } else if (first0Index != -1) {
+                int value = table[j][i];
+                table[j][i] = 0;
+                table[first0Index][i] = value;
+                first0Index -= 1;
+            }
+        }
+    }    
+    for (int j = 0; j < table[0].size(); j++) {
+        for (int i = table.size() - 1; i > 0; i--) {
+            if (table[i][j] != 0 && table[i-1][j] == table[i][j]) {
+                int value = table[i][j] * 2;
+                table[i-1][j] = 0;
+                table[i][j] = value;
+            }
+        }
+    }
+}
+
+void moveUp() {
+    for (int i = 0; i < 4; i++) {
+        int first0Index = -1;
+        for (int j = 0; j < 4; j++) {
+            if (table[j][i] == 0) {
+                if (first0Index == -1) {
+                    first0Index = j;
+                }
+            } else if (first0Index != -1) {
+                int value = table[j][i];
+                table[j][i] = 0;
+                table[first0Index][i] = value;
+                first0Index += 1;
+            }
+        }
+    }
+    for (int j = 0; j < table[0].size(); j++) {
+        for (int i = 0; i < table.size() - 1; i++) {
+            if (table[i][j] != 0 && table[i+1][j] == table[i][j]) {
+                int value = table[i][j] * 2;
+                table[i+1][j] = 0;
+                table[i][j] = value;
+            }
+        }
+    }
 }
 
 string getText(string caractere, int max) {
@@ -74,87 +189,16 @@ int getMaxTextLenght() {
     return to_string(k).size();
 }
 
-void moveLeft() {
-    for (int i = 0; i < table.size(); i++) {
-        int first0Index = -1;
-        for (int j = 0; j < table[i].size(); j++) {
-            if (table[i][j] == 0) {
-                if (first0Index == -1) {
-                    first0Index = j;
-                }
-            } else if (first0Index != -1) {
-                int value = table[i][j];
-                table[i][j] = 0;
-                table[i][first0Index] = value;
-                first0Index += 1;
-            }
-        }
-    }
-}
-
-void moveRight() {
-    for (int i = 0; i < table.size(); i++) {
-        int first0Index = -1;
-        for (int j = table[i].size() - 1; j >= 0; j--) {
-            if (table[i][j] == 0) {
-                if (first0Index == -1) {
-                    first0Index = j;
-                }
-            } else if (first0Index != -1) {
-                int value = table[i][j];
-                table[i][j] = 0;
-                table[i][first0Index] = value;
-                first0Index -= 1;
-            }
-        }
-    }
-}
-
-void moveDown() {
-    for (int i = 0; i < 4; i++) {
-        int first0Index = -1;
-        for (int j = 3; j >= 0; j--) {
-            if (table[j][i] == 0) {
-                if (first0Index == -1) {
-                    first0Index = j;
-                }
-            } else if (first0Index != -1) {
-                int value = table[j][i];
-                table[j][i] = 0;
-                table[first0Index][i] = value;
-                first0Index -= 1;
-            }
-        }
-    }    
-}
-
-void moveUp() {
-    for (int i = 0; i < 4; i++) {
-        int first0Index = -1;
-        for (int j = 0; j < 4; j++) {
-            if (table[j][i] == 0) {
-                if (first0Index == -1) {
-                    first0Index = j;
-                }
-            } else if (first0Index != -1) {
-                int value = table[j][i];
-                table[j][i] = 0;
-                table[first0Index][i] = value;
-                first0Index += 1;
-            }
-        }
-    }
-}
-
 void printConsole() {
     cout << endl;
     int longMax = getMaxTextLenght();
     for (int i = 0; i < table.size(); i++) {
         for (int j = 0; j < table[i].size(); j++) {
-            cout << " " << getText(to_string(table[i][j]), longMax) << " ";
+            cout << "* " << getText(to_string(table[i][j]), longMax) << " ";
         }
-        cout << endl;
+        cout << "*" << endl;
     }
+    cout << endl;
 }
 
 void start() {
@@ -162,7 +206,7 @@ void start() {
     for (int i = 0; i < 4; i++) {
         table[i] = {0, 0, 0, 0};
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 2; i++) {
         setRandomElement();
     }
     while (true) {
@@ -178,7 +222,11 @@ void start() {
             moveUp();
         } else if (answer == "b") {
             moveDown();
+        } else if (answer == "stop") {
+            cout << "Stop game." << endl;
+            break;
         }
+        setRandomElement();
     }
     
 }

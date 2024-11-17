9868784c7c47fce888e614e64d29926b27e1be3c
AlexPSvet <alejandroperezsvet@gmail.com>
2024-11-17 13:32:40 +0100

    origin/main

--- src/2048.cpp
+++ src/2048.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include "modele.h"
@@ -8,13 +9,13 @@ using namespace std;
 std::vector<std::vector<int>> table;
 
 /** Fonction pour afficher un tableau d'entiers sur la console.
- * @param t un tableau d'entiers.
+ * @param line un tableau d'entiers.
  **/
-void printTable(vector<int> table) {
+void printLine(const vector<int>& line) {
     cout << "[";
     for (int i = 0; i < table.size(); i++) {
-        cout << i;
-        if (i != table.size() - 1) {
+        cout << line[i];
+        if (i != line.size() - 1) {
             cout << ", ";
         }
     }
@@ -24,10 +25,10 @@ void printTable(vector<int> table) {
 /** Fonction pour afficher un tableau d'entiers à deux dimensions sur la console.
  * @param t un tableau d'entiers à deux dimensions
  **/
-void printTable(vector<vector<int>> table) {
+void printTable(const vector<vector<int>>& table) {
     cout << "[";
     for (int i = 0; i < table.size(); i++) {
-        printTable(table[i]);
+        printLine(table[i]);
         if (i != table.size() - 1) {
             cout << ", ";
         }
@@ -54,38 +55,25 @@ vector<tuple<int, int>> getEmptySlots() {
  */
 void setRandomElement() {
     vector<tuple<int, int>> emptySlots = getEmptySlots();
-    int k = (int) rand() % emptySlots.size();
-    tuple<int, int> values = emptySlots[k];
-    int r = rand() % 9;
+    int k = (int) rand() % emptySlots.size(); /// Choisit un tuple de facon aléatoire
+    tuple<int, int> values = emptySlots[k]; // Choisit les indices d'une case vide de ce sous-tableau de facon aléatoire
+    int r = rand() % 10; // 90% de Chance d'Ajouter un 2, 10% de Chance d'ajouter un 4.
     table[get<0>(values)][get<1>(values)] = r < 9 ? 2 : 4;
-}
-
-/** Fonction pour ajuster une chaîne de caracteres d'une taille spécifique.
- * @param caractere la chaîne de caracteres.
- * @param t la taille que la chaîne de caractères doi avoir
- * @return la chaîne de caractères avec la taille.
- **/
-string getText(string caractere, int t) {
-    string texte = caractere;
-    for (int i = 0; i < t - caractere.size(); i++) {
-        texte += ' ';
-    }
-    return texte;
 }
 
 /** Retrouve le nombre le plus grand en caractères.
  * @return la plus grande taille des numéros en chaîne de caractères.
  **/
 int getMaxTextLenght() {
-    int k = 0;
+    int maxValue = 0;
     for (int i = 0; i < table.size(); i++) {
         for (int j = 0; j < table[i].size(); j++) {
-            if(k < table[i][j]) {
-                k = table[i][j];
+            if (table[i][j] > maxValue) {
+                maxValue = table[i][j];
             }
         }
     }
-    return to_string(k).size();
+    return to_string(k).size(); // Renvoie la longueur de la chaîne de caracteres equvialente a numéro
 }
 
 /** Affiche dans la console le tableau à deux dimensions. 
@@ -95,56 +83,60 @@ void printConsole() {
     int longMax = getMaxTextLenght();
     for (int i = 0; i < table.size(); i++) {
         for (int j = 0; j < table[i].size(); j++) {
-            cout << "* " << getText(to_string(table[i][j]), longMax) << " ";
+            string s = to_string(table[i][j]);
+            s.resize(longMax, " ");
+            cout << "* " << setw(longMax) << s << " "; //Utilisation setw
         }
         cout << "*" << endl;
     }
     cout << endl;
 }
 
+/** Vérifier que la commande est valide à un type de mouvement.
+ * @param answer la réponse de l'utilisateur.
+ **/
+void verifyAnswer(string answer) {
+    while (answer != "d" && answer != "g" && answer != "h" && answer != "b") {
+        cout << "Votre commande est invalide." << endl;
+        cout << "Mouvemant haut : h\nMouvemant bas : b\nMouvemant gauche : g\nMouvemant droite : d\n";
+        cout << "Saisir une valeur de mouvement : ";
+        cin >> answer;
+    }
+}
+
 /** Vérifie que la commande de l'utilisateur est un mouvement valide, et réalise le mouvement.
  * @param answer la réponse en string de l'utilisateur en commande.
  **/
-bool doMovement(string answer) {
+void doMovement(string answer) {
+    verifyAnswer(answer);
     if (answer == "g") {
-        if (canMoveLeft()) {
+       if (canMoveLeft()) {
             moveLeft();
-            return true;
+            return;
         }
      } else if (answer == "d") {
         if (canMoveRight()) {
             moveRight();
-            return true;
+            return;
         }
     } else if (answer == "h") {
         if (canMoveUp()) {
             moveUp();
-            return true;
+            return;
         }
     } else if (answer == "b") {
         if (canMoveDown()) {
             moveDown();
-            return true;
+            return;
         }
-    } else {
-        cout << endl << "Votre commande est invalide." << endl;
-        cout << "Mouvemant haut : h" << endl;
-        cout << "Mouvemant bas : b" << endl;
-        cout << "Mouvemant gauche : g" << endl;
-        cout << "Mouvemant droite : d" << endl;
-        return false;
     }
     cout << "Le mouvement est invalide, saisir un mouvement valide." << endl;
-    return false;
 }
 
 /** Commence le jeu en initialisant le tableau, rajoute deux valeurs aléatoires et demande à l'utilisateur les mouvements. 
  **/
 void start() {
-    table = vector<vector<int>>(4);
-    for (int i = 0; i < 4; i++) {
-        table[i] = {0, 0, 0, 0};
-    }
+    table = vector<vector<int>>(4, vector<int>(4, 0));
     for (int i = 0; i < 2; i++) {
         setRandomElement();
     }
